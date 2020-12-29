2737041980909a406248285bb3a766afbf436c69
ihorstepura <igor.stepura96@gmail.com>
2020-12-29 23:26:33 +0200

    LJ-2 added trip-selector.component into app package

---
+++ src/app/trip-selector/trip-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-trip-selector',
+    templateUrl: './trip-selector.component.html',
+    styleUrls: ['./trip-selector.component.scss'],
+})
+export class TripSelectorComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
