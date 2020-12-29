7c5ba9cc94f69fbe9cdfceb5a269fb8de1795c24
ihorstepura <igor.stepura96@gmail.com>
2020-12-29 23:21:20 +0200

    LJ-2 added route-selector.component to ui package

---
+++ src/app/ui/route-selector/route-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-route-selector',
+    templateUrl: './route-selector.component.html',
+    styleUrls: ['./route-selector.component.scss'],
+})
+export class RouteSelectorComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
