2cb0193cb3ca3597e5d3fe32fb20e4c2a3230f65
ihorstepura <igor.stepura96@gmail.com>
2020-12-29 23:29:08 +0200

    LJ-2 declared new components in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TripSelectorComponent } from './trip-selector/trip-selector.component';
+import { SelectorComponent } from './ui/selector/selector.component';
+import { InputComponent } from './ui/input/input.component';
+import { DatePickerComponent } from './ui/date-picker/date-picker.component';
+import { RouteSelectorComponent } from './ui/route-selector/route-selector.component';
+import { CostInputComponent } from './ui/cost-input/cost-input.component';
+
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        TripSelectorComponent,
+        SelectorComponent,
+        InputComponent,
+        DatePickerComponent,
+        RouteSelectorComponent,
+        CostInputComponent,
+    ],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
